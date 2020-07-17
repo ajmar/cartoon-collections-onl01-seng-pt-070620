b9cb4d10b07afabdc28b1d69cc4e42d638a1b0b4
Alexander Marusin <amarusin18@gmail.com>
2020-07-17 20:01:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,8 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(string)
-  if string.include?("cheddar", "gouda", "camembert")
-    return string(1)
+  if string.include?("cheddar")("gouda")("camembert")
+    return string
   else
     puts "None"
   end
